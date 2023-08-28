930c06d5d03c9ed68851b787d5a76cc9cb510051
jbikeler <jbikeler@gmail.com>
2023-08-28 12:41:54 -0700

    Updated Annual Salary Math Equation

--- AnonymousIncome/AnonymousIncome/AnonymousIncome/Program.cs
+++ AnonymousIncome/AnonymousIncome/AnonymousIncome/Program.cs
@@ -16,9 +16,9 @@ namespace AnonymousIncome
             int p2Rate = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter Person 2 hours worked per week: ");
             int p2Hours = Convert.ToInt32(Console.ReadLine());
-            int p1Salary = p1Hours * p1Rate * 4 * 12; // find Person 1 Salary
+            int p1Salary = p1Hours * p1Rate * 52; // find Person 1 Salary
             Console.WriteLine("Annual salary of Person 1: " + Convert.ToString(p1Salary));
-            int p2Salary = p2Hours * p2Rate * 4 * 12; // find Person 2 Salary
+            int p2Salary = p2Hours * p2Rate * 52; // find Person 2 Salary
             Console.WriteLine("Annual salary of Person 2: " + Convert.ToString(p2Salary));
             Console.WriteLine("Does Person 1 make more money than Person 2? " + Convert.ToString(p1Salary > p2Salary)); // get Person 1 salary is greater than Person 2
         }
