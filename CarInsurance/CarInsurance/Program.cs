bf2491cbfad69200e5e8b6a0e4b3cff0f217df24
jbikeler <jbikeler@gmail.com>
2023-08-28 17:17:22 -0700

    Updated Car Insurance

--- CarInsurance/CarInsurance/Program.cs
+++ CarInsurance/CarInsurance/Program.cs
@@ -13,8 +13,8 @@ namespace CarInsurance
             bool DUI = Convert.ToBoolean(Console.ReadLine()); // does user have DUI?
             Console.WriteLine("How many speeding tickets do you have?");
             int tickets = Convert.ToInt32(Console.ReadLine()); // get amount of tickets given
-            Console.WriteLine("Qualified? ");
-            Console.WriteLine(Convert.ToString(age > 15 ^ DUI && tickets <= 3)); // if user age is over 15, they don't have a DUI and they have < 3 tickets then they qualify
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(Convert.ToString(age > 15 && DUI != true && tickets <= 3)); // if user age is over 15, they don't have a DUI and they have < 3 tickets then they qualify
         }
     }
 }
