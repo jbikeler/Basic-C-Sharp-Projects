57497fdb6023505057d397a7efff407f241bd627
jbikeler <jbikeler@gmail.com>
2023-08-30 14:06:18 -0700

    Update Program.cs

--- PackageExpress/PackageExpress/Program.cs
+++ PackageExpress/PackageExpress/Program.cs
@@ -9,11 +9,11 @@ namespace PackageExpress
             Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
             Console.WriteLine("What is the weight of your package?");
             int weight = Convert.ToInt32(Console.ReadLine());
-            if (weight > 50)
+            if (weight > 50) // ask weight
             {
-                Console.WriteLine("Package too heavy to be shipped via Package Express.Have a good day."); 
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day."); 
             }
-            else
+            else // if package is 50 or under check the dimensions
             {
                 Console.WriteLine("What is the width of your package?");
                 int width = Convert.ToInt32(Console.ReadLine());
@@ -21,11 +21,11 @@ namespace PackageExpress
                 int height = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("What is the length of your package?");
                 int length = Convert.ToInt32(Console.ReadLine());
-                if (width > 50 || height > 50 || length > 50)
+                if (width > 50 || height > 50 || length > 50) // is package small enough?
                 {
                     Console.WriteLine("Package too big to be shipped via Package Express.");
                 }
-                else
+                else // if the package is small enough then offer quote
                 {
                     float quote = (height * width * length) * weight / 100;
                     Console.WriteLine("Your estimated total for shipping this package is: " + quote.ToString("c2"));
