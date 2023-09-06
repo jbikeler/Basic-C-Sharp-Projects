fb9868a319f53d2f70f23bab0b004bef00d1b43f
jbikeler <jbikeler@gmail.com>
2023-09-06 14:50:16 -0700

    Added MethodClass

---
+++ MethodClass/MethodClass/MyClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodClass
+{
+    class MyClass
+    {
+        public void method1(int intOne, int intTwo) // create a void method that takes two integers as parameters
+        {
+            int mathOp = intOne + 5; // do a math operation on the first integer
+            Console.WriteLine(intTwo); // display the second integer to the screen
+        }
+    }
+}
---
+++ MethodClass/MethodClass/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MethodClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myObj = new MyClass(); // instantiate the class
+            myObj.method1(2, 5); // Call the method in the class, passing in two numbers
+            myObj.method1(intOne: 2, intTwo: 5); // Call the method in the class, specifying the parameters by name
+        }
+    }
+}
