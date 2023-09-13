17a81cb13fc6627f66d78efed0d4bbfbbecef7f5
jbikeler <jbikeler@gmail.com>
2023-09-13 13:26:32 -0700

    Added DateTime_1

---
+++ DateTime_1/DateTime_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DateTime_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime rightNow = DateTime.Now;
+            Console.WriteLine(rightNow);
+            Console.WriteLine("Enter in a number of your choosing.");
+            TimeSpan userInput = new TimeSpan(Convert.ToInt32(Console.ReadLine()), 0, 0);
+            DateTime newDate = rightNow.Add(userInput);
+            Console.WriteLine($"{rightNow} + {userInput} hours = {newDate}");
+        }
+    }
+}
--- TwentyOne/TwentyOne/Dealer.cs
+++ TwentyOne/TwentyOne/Dealer.cs
@@ -19,6 +19,7 @@ namespace TwentyOne
             Console.WriteLine(card);
             using (StreamWriter file = new StreamWriter(@"C:\Users\jbike\OneDrive\Desktop\MyText.txt", true))
             {
+                file.WriteLine(DateTime.Now);
                 file.WriteLine(card);
             }
             Deck.Cards.RemoveAt(0);
