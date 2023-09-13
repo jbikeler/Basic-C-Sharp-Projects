b9c1a5b915c4276a2f4a6e8f45be575d7067e871
jbikeler <jbikeler@gmail.com>
2023-09-13 13:29:28 -0700

    Updated DateTime_1 with Comments

--- DateTime_1/DateTime_1/Program.cs
+++ DateTime_1/DateTime_1/Program.cs
@@ -6,12 +6,12 @@ namespace DateTime_1
     {
         static void Main(string[] args)
         {
-            DateTime rightNow = DateTime.Now;
-            Console.WriteLine(rightNow);
-            Console.WriteLine("Enter in a number of your choosing.");
-            TimeSpan userInput = new TimeSpan(Convert.ToInt32(Console.ReadLine()), 0, 0);
-            DateTime newDate = rightNow.Add(userInput);
-            Console.WriteLine($"{rightNow} + {userInput} hours = {newDate}");
+            DateTime rightNow = DateTime.Now; // Create an object to date time with the value of now 
+            Console.WriteLine(rightNow); // Print that to the user 
+            Console.WriteLine("Enter in a number of your choosing. This number will represent hours."); // Ask the user to choose an amount of hours 
+            TimeSpan userInput = new TimeSpan(Convert.ToInt32(Console.ReadLine()), 0, 0); // Convert the users input to a timespan object 
+            DateTime newDate = rightNow.Add(userInput); // Add the users input to the current date 
+            Console.WriteLine($"{rightNow} + {userInput} hours = {newDate}"); // Print the results for the user to see 
         }
     }
 }
