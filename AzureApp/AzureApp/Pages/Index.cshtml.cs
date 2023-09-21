29de4ab59eb0f9bed030b413b5d70c8b86eb6649
jbikeler <jbikeler@gmail.com>
2023-09-21 13:10:20 -0700

    Added AzureApp

---
+++ AzureApp/AzureApp/Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AzureApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            DateTime serverTime = DateTime.Now;
+            ViewData["NowTime"] = serverTime.ToString();
+        }
+    }
+}
